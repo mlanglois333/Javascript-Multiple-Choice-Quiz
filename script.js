d474e5437a10765a8fa5c8610a7668f0d74d35a2
Marc <mlanglois333@icloud.com>
2020-07-10 09:31:50 -0400

    created main divs

---
+++ script.js
@@ -0,0 +1,5 @@
+var mainBody = document.getElementById("#main");
+var timer = document.getElementById("#timer");
+var score = document.getElementById("#score");
+var submitBtn = document.getElementById("#submit");
+
