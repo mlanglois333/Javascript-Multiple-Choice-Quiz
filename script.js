9ca2bb059d3f822f1c0db85ad792163e6232caa0
Marc <mlanglois333@icloud.com>
2020-07-13 16:11:11 -0400

    wrote render and next function

--- script.js
+++ script.js
@@ -2,69 +2,82 @@ var mainBody = document.querySelector("#main");
 var timerDisp = document.querySelector("#timer");
 var scoreDisp = document.querySelector("#score");
 var startBtn = document.querySelector("#startQuiz");
-var questions = document.querySelector("questions");
-var answers = document.querySelector("answers");
-var answerA = document.querySelector("A");
-var answerB = document.querySelector("B");
-var answerC = document.querySelector("C");
-var answerD = document.querySelector("D");
-
+var questionEl = document.getElementById("questions");
+var answers = document.getElementById("answers");
+var answerA = document.getElementById("a");
+var answerB = document.getElementById("b");
+var answerC = document.getElementById("c");
+var answerD = document.getElementById("d");
+let indexNo = 0;
 var questions = [
     { 
         question: "q1",
-        answers: {
+        
             a: "a",
             b: "b",
             c: "c",
-            b: "d",
-        },
+            d: "d",
+       
         correct: "a"
     },
     { 
         question: "q2",
-        answers: {
+        
             a: "a",
             b: "b",
             c: "c",
-            b: "d",
-        },
+            d: "d",
+        
         correct: "a"
     },
     { 
         question: "q3",
-        answers: {
+        
             a: "a",
             b: "b",
             c: "c",
-            b: "d",
-        },
+            d: "d",
+        
         correct: "a"
     },
     { 
         question: "q4",
-        answers: {
+        
             a: "a",
             b: "b",
             c: "c",
-            b: "d",
-        },
+            d: "d",
+        
         correct: "a"
     },
     { 
         question: "q5",
-        answers: {
+        
             a: "a",
             b: "b",
             c: "c",
-            b: "d",
-        },
+            d: "d",
+        
         correct: "a"
     }
 ];
+let arrLength = questions.length ;
 
 function quiz() {
 
+    let qu = questions[indexNo];
+    questionEl.innerHTML = "<p>" + qu.question + "</p>";
+    answerA.innerHTML = "<p>" +qu.a + "</p>";
+    answerB.innerHTML = "<p>" +qu.b + "</p>";
+    answerC.innerHTML = "<p>" +qu.c + "</p>";
+    answerD.innerHTML = "<p>" +qu.d + "</p>";
 
+}
+
+function nextQ(){
+
+    indexNo++;
+    quiz();
 
 }
 
@@ -96,8 +109,5 @@ if (secondsRemaining > 0) {
 
 
 
-
-startBtn.addEventListener("click", timer());
-
-
+quiz();
 
